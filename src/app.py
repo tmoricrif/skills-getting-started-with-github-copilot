09314aeacc5e84b0d532f480ce00819052554101
tmoricrif <t.mori@contractors.crifgroup.com>
2025-10-16 14:10:24 +0000

    Aggiungi la validazione per evitare l'iscrizione a un'attività piena

--- src/app.py
+++ src/app.py
@@ -66,7 +66,10 @@ def signup_for_activity(activity_name: str, email: str):
     if email in activity["participants"]:
         raise HTTPException(
             status_code=400, detail="Student already signed up for this activity")
-    
+    # Validate activity is not full
+    if len(activity["participants"]) >= activity["max_participants"]:
+        raise HTTPException(status_code=400, detail="Activity is full")
+
     # Add student
     activity["participants"].append(email)
     return {"message": f"Signed up {email} for {activity_name}"}
